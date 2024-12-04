b01a74777989e441af155650027394bcf20772eb
Jyotheeswar198 <jyotheeswar146@gmail.com>
2024-12-04 18:15:09 +0530

    passing the information of array

---
+++ ARRAY/27-ARRAY.cpp
@@ -0,0 +1,15 @@
+/*#include<stdio.h>
+void main()
+{
+	int a[10]={60,70,80,90,160};	
+
+	printf("%d %d %d %d",a[0],a[1],a[2],a[3],a[4]);
+}*/
+
+#include<stdio.h>
+void main()
+{
+	int a[10]={62,545,55,555};
+	printf("%d",a[2]);
+
+//}
